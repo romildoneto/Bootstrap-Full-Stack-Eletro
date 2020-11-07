2bf3ef47bcfcf18bb2aea0fe6ae2959396dcb284
romildoneto <r7746@hotmail.com>
2020-11-06 22:03:13 -0300

    meu primeiro commit bootstrap para o github

--- Fale_Conosco.php
+++ Fale_Conosco.php
@@ -29,8 +29,11 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Contato-Full Stack Eletro</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Full Stack Eletro</title>
     <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
 </head>
 
 <body>
@@ -43,46 +46,51 @@
 
     <br> <br>
 
-    <div onmouseover="alterarcor(this)" onmouseout="alterarcor(this)" class="elementoEmail">
-        <img src="./imagens/logoEmail.JPEG" width="50px" onclick="foto(this)">
-        contato@fullstackeletro.com
-    </div>
 
-    <div onmouseover="alterarcor(this)" onmouseout="alterarcor(this)" class="elementoContato">
-        <img src="imagens/zap2.jpeg" width="120px" onclick="foto(this)">
-        (11) 89889-4566
-    </div>
+ <div class="row ml-5">
+    <div class="col"><img src="./imagens/email.jpeg" width="50px"></div>
+    <div class="col"><img src="./imagens/zap2." width="50px"></div>
+  </div>
 
+ <div class="row ml-5">
+    <div class="col" onmouseover="changecolor(this)" onmouseout="changecolor(this)">contato@fullstackeletro.com</div>
+    <div class="col" onmouseover="changecolor(this)" onmouseout="changecolor(this)">(11)99999-9999</div>
+ </div>
 
-        <br> <br><br> <br>
+        <br> <br>
 
 
  <form method ="POST" action ="">
     
-       
-<h4 style="margin-left:3%">Nome</h4>
-<input type="text" name="nome" class="inputname">
-<h4 style="margin-left:3%">Mensagem</h4>
-<input type="text" name="mensagem" class="inputmensagem"> <br> <br>
-<input type="submit" class="inputentrada" name="submit" value="enviar">
+ <form method="post" action="">
+  <div class="form-group m-4">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" class="form-control w-50 p-3" id="nome" placeholder="Digite seu nome">
+  </div>
 
 
-
-
-
-
+  <div class="form-group m-4">
+    <label for="msg">Mensagem</label>
+    <input type="text" name="mensagem" class="form-control w-50 p-3" id="mensagem" placeholder="Digite sua mensagem...">
+    </div>
+    <button type="submit" class="btn btn-danger  m-4">Enviar</button>
  </form>
 
-    <br><br>
+    <br>
 
-    <footer class="rodape">
-        <p onmouseover="alterarcor(this)"onmouseout="alterarcor(this)" class="fcartao"><b>Formas de pagamento:</b></p>
-        <br><br><br><br>
-        <img src="imagens/imagem3.png" alt="imagem3"><br>
-        <p>&copy;Recode</p>
-    </footer>
+    <div class="container-center" style="text-align:center;">
+      <footer>
+         <p class="text-danger" onmouseover="changecolor(this)" onmouseout="changecolor(this)"><b>Formas de pagamento:</b></p>
+         <img id="cartcredito" src="imagens/imagem3.png" alt="imagem3" onclick="ftcredito(this)"><br>
+         <p>&copy;Recode</p>
+       </footer>
+    </div>
    
-    <script src="./javaScript/funcoes.js"></script>
+   
+    <script src="./teste.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script
 
 </body>
 </html>
--- pedidos.php
+++ pedidos.php
@@ -33,8 +33,10 @@ $result = $conn->query($sql);
 
 <head>
     <meta charset="utf-8">
-    <title>Pedido-Full Stack Eletro</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Full Stack Eletro</title>
     <link rel="stylesheet" href="./css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     
 </head>
 
@@ -48,41 +50,51 @@ include_once("menu.html");
     <!-- Inicio do Menu-->
     <br> 
 
-<form method = 'POST' name = 'pedidos' action = "" >
+<form method ='POST' name = 'pedidos' action = "">
+<form method="post" action="">
 
     <h1 id="tituloPedido">Faça seu pedido já:</h1>
         <div class="inputPedido">
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)"> Meu nome:</h4>
-        <input type="text" name= "nomeCliente" placeholder="digite seu nome" class="inputform" > <br>
+        <input type="text" name= "nomeCliente"class="form-control w-130 p-3" placeholder="digite seu nome" class="inputform" > <br>
         
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)"> Endereço:</h4>
-        <input type="text" name= "endereco" placeholder="digite seu endereço" class="inputform" > <br>
+        <input type="text" name= "endereco" class="form-control w-130 p-3"placeholder="digite seu endereço" class="inputform" > <br>
        
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)"> Telefone:</h4>
-        <input type="text" name= "telefone" placeholder="Digite seu telefone" class=" inputform"> <br>
+        <input type="text" name= "telefone"class="form-control w-130 p-3" placeholder="Digite seu telefone" class=" inputform"> <br>
         
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)"> Produto:</h4>
-        <input type="text" name= "nomeProduto" placeholder="digite seu produto" class="inputform" > <br>
+        <input type="text" name= "nomeProduto" class="form-control w-130 p-3"placeholder="digite seu produto" class="inputform" > <br>
         
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)"> R$ Inicial:</h4>
-        <input type="text" name= "valorUnitario" placeholder="digite o valor" class="inputform" > <br>
+        <input type="text" name= "valorUnitario"class="form-control w-130 p-3" placeholder="digite o valor" class="inputform" > <br>
        
         <h4 onmouseover="alterarcor(this)" onmouseout="alterarcor(this)">Quantidade:</h4>
-        <input type="text" name="quantidade" placeholder="digite a quantidade" class="inputform" > <br>
+        <input type="text" name="quantidade" class="form-control w-130 p-3"placeholder="digite a quantidade" class="inputform" > <br>
        
         <h4 onmouseover="alterarcor(this)" onmouseout="alterar]]cor(this)">R$ Total:</h4>
-        <input type="text" name="valorTotal" number="numero" placeholder="digite valor total" class="inputform" > <br><br>
+        <input type="text" name="valorTotal" number="numero"class="form-control w-130 p-3" placeholder="digite valor total" class="inputform"> <br>
        
-        <input type="submit" name="submit" value="ENVIAR!" class=" inputEnviar">
+        <input type="submit" name="submit" value="ENVIAR!" class="btn btn-danger m-0  form-control w-130 p-3 text-align:center"  class=" inputEnviar">
  </form>
  
-    <footer class="rodape">
+<br><br>
+    <footer class="rodape text-align:center">
+       <div class="jumbotron">
         <p onmouseover="alterarcor(this)" onmouseout="alterarcor(this)" class="fcartao"><b>Formas de pagamento:</b></p>
-        <br><br>
         <img src="imagens/imagem3.png" alt="imagem3">
         <p>&copy;Recode</p>
+        </div>
     </footer>
+   
+   
+   
     <script src="./teste.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script
+
 </body>
 
 
